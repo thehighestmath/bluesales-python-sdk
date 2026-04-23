b826f4feffaaf428a3877ce50bff37802bce1bb3
thehighestmath <kirill.lfybk.rh@gmail.com>
2026-04-23 12:11:22 +0300

    fix lint

--- bluesalespy/__init__.py
+++ bluesalespy/__init__.py
@@ -6,3 +6,15 @@ __api_version__ = '1.0'
 from bluesalespy.bluesales import BlueSales
 from bluesalespy.exceptions import BlueSalesError, HttpError, TooLargeBoarders, WrongLoginOrPassword
 from bluesalespy.models import Customer, CustomersResponse, Order, OrdersResponse
+
+__all__ = [
+    'BlueSales',
+    'BlueSalesError',
+    'Customer',
+    'CustomersResponse',
+    'HttpError',
+    'Order',
+    'OrdersResponse',
+    'TooLargeBoarders',
+    'WrongLoginOrPassword',
+]
